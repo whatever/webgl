085f922e529fd2a67500ce46500f2aebeaf2c0ad
Matt <mowen@iq.harvard.edu>
2013-11-07 15:17:52 -0500

    Rotation control instead of auto rotate

--- t5-001/app.js
+++ t5-001/app.js
@@ -106,7 +106,7 @@ var app = function (_canvasId) {
     mat4.identity(webgl.mvMatrix);
     mat4.translate(webgl.mvMatrix, [ 0, 0, -_controls.z_translate ]);
     mat4.rotate(webgl.mvMatrix, Math.PI/2, [ 1, 0, 0 ]);
-    mat4.rotate(webgl.mvMatrix, t, [ 0, 0, -1 ]);
+    mat4.rotate(webgl.mvMatrix, _controls.rotate, [ 0, 0, -1 ]);
 
     for (var i = 0; i < _planes.length; i++) {
       _gl.enable(_gl.BLEND);
